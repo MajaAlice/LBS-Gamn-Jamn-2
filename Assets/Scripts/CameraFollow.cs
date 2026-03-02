3ff15230fab782e587f4657428513d3987185b09
MajaAlice <filip.franas@elev.ga.lbs.se>
2026-03-02 13:15:44 +0100

    Player Movement and camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    //maja
+    GameObject Player;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -10);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
 
 public class Player : MonoBehaviour
 {
+
+    InputAction DirectionChange;
+    InputAction Thrust;
+    Rigidbody2D rb;
+
+    public float rotationSpeed = 3;
+    public float thrust = 2;
+
     void Start()
     {
-        
-    }
+        #region refrences
+        //as of rn the input system is bugged and doesnt save settings, so iäm using default settigns -- maja
+        DirectionChange = InputSystem.actions.FindAction("Move");
+        Thrust = InputSystem.actions.FindAction("Jump");
 
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        #endregion
+    }
+        
     private void FixedUpdate()
     {
-        
+        RotatePlayer();
+        MoveForward();
+    }
+
+
+    //maja
+    void RotatePlayer()
+    {
+        float direction = DirectionChange.ReadValue<Vector2>().x * -1;
+        transform.rotation *= Quaternion.AngleAxis(direction * rotationSpeed * Time.fixedDeltaTime, Vector3.forward);
+    }
+
+    void MoveForward()
+    {
+        if (Thrust.ReadValue<float>() == 1)
+        {
+            rb.linearVelocity += new Vector2(transform.up.x, transform.up.y) * thrust * Time.fixedDeltaTime;
+            Debug.Log("buh");
+        }
     }
 
 }
