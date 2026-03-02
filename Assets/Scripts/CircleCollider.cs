ddbbeabf8a06a4b910b4f263516715fa7a1d3482
DudLudNil64 <ludwig.gothbergmeiton@elev.ga.lbs.se>
2026-03-02 14:32:29 +0100

    Added Trushtjgnbsiojdghksrg yea

--- Assets/Scripts/CircleCollider.cs
+++ Assets/Scripts/CircleCollider.cs
@@ -6,6 +6,8 @@ public class CircleCollider : MonoBehaviour
     GameObject Player;
     CircleCollider2D PlayerCollider;
     Rigidbody2D PlayerBody;
+    Player PlayerMain;
+
     public float outerCircle;
     public float innerCircle;
     public float bouncy;
@@ -15,6 +17,7 @@ public class CircleCollider : MonoBehaviour
         Player = GameObject.FindGameObjectWithTag("Player");
         PlayerCollider = Player.GetComponent<CircleCollider2D>();
         PlayerBody = Player.GetComponent<Rigidbody2D>();
+        PlayerMain = Player.GetComponent<Player>();
     }
 
     private void FixedUpdate()
@@ -22,8 +25,10 @@ public class CircleCollider : MonoBehaviour
         // Converts To Local Space AKA The 0,0 Is Now The Center Of Scripts Pearent -Lud
         Vector3 localPlayerPos = Player.transform.position - gameObject.transform.position;
 
+        // Checks If It Should Check
         if (localPlayerPos.magnitude < outerCircle)
         {
+            // Checks If The Player Is Outside The Outer Circle
             if (localPlayerPos.magnitude > (outerCircle - PlayerCollider.radius))
             {
                 localPlayerPos.Normalize();
@@ -33,6 +38,7 @@ public class CircleCollider : MonoBehaviour
                 PlayerBody.linearVelocity += localDot;
                 PlayerBody.linearVelocity *= bouncy;
             }
+            // Checks If The Player Is In The InnerCircle -Lud
             else if (localPlayerPos.magnitude < (innerCircle + PlayerCollider.radius))
             {
                 localPlayerPos.Normalize();
@@ -43,14 +49,24 @@ public class CircleCollider : MonoBehaviour
                 PlayerBody.linearVelocity *= bouncy;
             }
 
+            // Compares To See What Direction Is Smaller -Lud
+            if ((outerCircle -localPlayerPos.magnitude) < (localPlayerPos.magnitude - innerCircle))
+            {
+                PlayerMain.SetThrust(outerCircle - localPlayerPos.magnitude);
+            }
+            else
+            {
+                PlayerMain.SetThrust(localPlayerPos.magnitude - innerCircle);
+            }
+
             Player.transform.position = localPlayerPos + gameObject.transform.position;
         }
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(Vector3.zero, outerCircle);
+        Gizmos.DrawWireSphere(gameObject.transform.position, outerCircle);
         Gizmos.color = Color.purple;
-        Gizmos.DrawWireSphere(Vector3.zero, innerCircle);
+        Gizmos.DrawWireSphere(gameObject.transform.position, innerCircle);
     }
 }
