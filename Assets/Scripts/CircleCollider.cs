4025ef07192192140226d0b7c0e39c5355f9d268
DudLudNil64 <ludwig.gothbergmeiton@elev.ga.lbs.se>
2026-03-03 10:09:00 +0100

    Added NOT Shitty method

--- Assets/Scripts/CircleCollider.cs
+++ Assets/Scripts/CircleCollider.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 
 public class CircleCollider : MonoBehaviour
 {
+    // Main Script Refrences -Lud
     GameObject Player;
     CircleCollider2D PlayerCollider;
     Rigidbody2D PlayerBody;
     Player PlayerMain;
 
-    public float outerCircle;
-    public float innerCircle;
-    public float bouncy;
+    // Main Script Data -Lud
+    public float outerCircle;// How Big The Outer Circle Is
+    public float innerCircle;// How Big The Inner Circle Is
+    public float bouncy;     // So That You Lose An Amount Of Energy
+    public Vector2 rotation; // For Rotation Check
 
     void Start()
     {
@@ -26,40 +29,49 @@ public class CircleCollider : MonoBehaviour
         Vector3 localPlayerPos = Player.transform.position - gameObject.transform.position;
 
         // Checks If It Should Check
-        if (localPlayerPos.magnitude < outerCircle)
+        if (localPlayerPos.magnitude < outerCircle + PlayerCollider.radius)
         {
-            // Checks If The Player Is Outside The Outer Circle
-            if (localPlayerPos.magnitude > (outerCircle - PlayerCollider.radius))
-            {
-                localPlayerPos.Normalize();
-                localPlayerPos *= outerCircle - PlayerCollider.radius;
-                Vector2 localDot = localPlayerPos * -1;
-                localDot.Normalize();
-                PlayerBody.linearVelocity += localDot;
-                PlayerBody.linearVelocity *= bouncy;
-            }
-            // Checks If The Player Is In The InnerCircle -Lud
-            else if (localPlayerPos.magnitude < (innerCircle + PlayerCollider.radius))
-            {
-                localPlayerPos.Normalize();
-                localPlayerPos *= innerCircle + PlayerCollider.radius;
-                Vector2 localDot = localPlayerPos;
-                localDot.Normalize();
-                PlayerBody.linearVelocity += localDot;
-                PlayerBody.linearVelocity *= bouncy;
-            }
+            Vector2 localDot = localPlayerPos;
+            localDot.Normalize();
 
-            // Compares To See What Direction Is Smaller -Lud
-            if ((outerCircle -localPlayerPos.magnitude) < (localPlayerPos.magnitude - innerCircle))
-            {
-                PlayerMain.SetThrust(outerCircle - localPlayerPos.magnitude);
-            }
-            else
-            {
-                PlayerMain.SetThrust(localPlayerPos.magnitude - innerCircle);
-            }
+            // Trust Me Its Not -Lud :3
+            bool notAShityWayToDoThis = false;
+            if ((rotation.x > 0) && (rotation.y > 0)) { notAShityWayToDoThis = (localDot.x > 0) && (localDot.y > 0); }
+            else if ((rotation.x > 0) && (rotation.y < 0)) { notAShityWayToDoThis = (localDot.x > 0) && (localDot.y < 0); }
+            else if ((rotation.x < 0) && (rotation.y > 0)) { notAShityWayToDoThis = (localDot.x < 0) && (localDot.y > 0); }
+            else if ((rotation.x < 0) && (rotation.y < 0)) { notAShityWayToDoThis = (localDot.x < 0) && (localDot.y < 0); }
 
-            Player.transform.position = localPlayerPos + gameObject.transform.position;
+            if (notAShityWayToDoThis)
+            {
+                // Checks If The Player Is Outside The Outer Circle
+                if (localPlayerPos.magnitude > (outerCircle - PlayerCollider.radius))
+                {
+                    localPlayerPos.Normalize();
+                    localPlayerPos *= outerCircle - PlayerCollider.radius;
+                    PlayerBody.linearVelocity += localDot;
+                    PlayerBody.linearVelocity *= bouncy;
+                }
+                // Checks If The Player Is In The InnerCircle -Lud
+                else if (localPlayerPos.magnitude < (innerCircle + PlayerCollider.radius))
+                {
+                    localPlayerPos.Normalize();
+                    localPlayerPos *= innerCircle + PlayerCollider.radius;
+                    PlayerBody.linearVelocity += localDot;
+                    PlayerBody.linearVelocity *= bouncy;
+                }
+
+                // Compares To See What Direction Is Smaller -Lud
+                if ((outerCircle - localPlayerPos.magnitude) < (localPlayerPos.magnitude - innerCircle))
+                {
+                    PlayerMain.SetThrust(outerCircle - localPlayerPos.magnitude);
+                }
+                else
+                {
+                    PlayerMain.SetThrust(localPlayerPos.magnitude - innerCircle);
+                }
+
+                Player.transform.position = localPlayerPos + gameObject.transform.position;
+            }
         }
     }
     private void OnDrawGizmos()
