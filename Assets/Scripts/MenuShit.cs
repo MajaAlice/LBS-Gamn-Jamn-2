7a7fc2f1afaf240269d2bde1f4f0f34cb8dc642a
DudLudNil64 <ludwig.gothbergmeiton@elev.ga.lbs.se>
2026-03-04 17:22:13 +0100

    WTF I HAVE THINGS

--- Assets/Scripts/MenuShit.cs
+++ Assets/Scripts/MenuShit.cs
@@ -1,6 +1,4 @@
-using NUnit.Framework;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MenuShit : MonoBehaviour
@@ -11,9 +9,49 @@ public class MenuShit : MonoBehaviour
     public GameObject MainMenu;
     public GameObject SettingsMenu;
     public GameObject LevelSelectMenu;
-    public GameObject PauseScreen;
+    public GameObject DeathMenu;
+    public GameObject VictoryMenu;
+    public GameObject Pause;
     // Levels -Lud
     public List<GameObject> Levels;
+
+    public enum Menus
+    {
+        NULL,
+        Main,
+        Settings,
+        LevelSelect,
+        Death,
+        Win,
+        Pause
+    }
+    // Used In Making Sure The Correct 2 UIs Changed -Lid -Lud
+    void ToggleDualUI(Menus TurnOff, Menus TurnOn)
+    {
+        switch(TurnOff)
+        {
+            case Menus.NULL:
+                break;
+            case Menus.Main:
+                MainMenu.SetActive(false);
+                break;
+            case Menus.Settings:
+                SettingsMenu.SetActive(false);
+                break;
+            case Menus.LevelSelect:
+                LevelSelectMenu.SetActive(false);
+                break;
+            case Menus.Death:
+                DeathMenu.SetActive(false);
+                break;
+            case Menus.Win:
+                VictoryMenu.SetActive(false);
+                break;
+            case Menus.Pause:
+                Pause.SetActive(false);
+                break;
+        }
+    }
 
     public void Quit() // Quits The Game -Lud
     {
@@ -24,13 +62,15 @@ public class MenuShit : MonoBehaviour
         MainMenu.SetActive(true);
         SettingsMenu.SetActive(false);
         LevelSelectMenu.SetActive(false);
-        PauseScreen.SetActive(false);
+        DeathMenu.SetActive(false);
     }
+    // Goes From Menu To Settings -Lud
     public void Settings()
     {
         MainMenu.SetActive(false);
         SettingsMenu.SetActive(true);
     }
+    // Loads The Level On The Holder And Turns Of UI .-.
     public void SelectLevel(int SelectedLevel)
     {
         LevelSelectMenu.SetActive(false);
@@ -43,7 +83,7 @@ public class MenuShit : MonoBehaviour
     }
     public void LevelSelectFromPauseScreen()
     {
-        PauseScreen.SetActive(false);
+        DeathMenu.SetActive(false);
         LevelSelectMenu.SetActive(true);
     }
 }
