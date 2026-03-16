f2b79a2e38c3fa6359cd3ab37d61fce68981b38f
DudLudNil64 <ludwig.gothbergmeiton@elev.ga.lbs.se>
2026-03-16 09:17:21 +0100

    idk
    this is stuff

--- Assets/Scripts/LineCollider.cs
+++ Assets/Scripts/LineCollider.cs
@@ -5,8 +5,31 @@ public class LineCollider : MonoBehaviour
 {
     // Dumb Float Making Curves Look Solid While They Have Gaps -Lud
     public static float LineExtraThick = 0.15f;
+    // Player Veriables -Lud
+    public GameObject PlayerObject;
+    public Rigidbody2D PlayerBody;
+    public CircleCollider2D PlayerCollider;
+    // Line Veriables -Lud
+    public float lenght;
+    public bool ColliderSide = true;
+    public Vector2 pointA;
+    public Vector2 pointB;
+    public Vector2 normal;
+    public static float rigidity;
+    public static float tangentRigidity;
+
+    private void Start()
+    {
+        PlayerObject = GameObject.FindGameObjectWithTag("Player");
+        if(PlayerObject != null)
+        {
+            PlayerBody = PlayerObject.GetComponent<Rigidbody2D>();
+            PlayerCollider = PlayerObject.GetComponent<CircleCollider2D>();
+        }
+    }
+
     // Makes Sure The Normals Are Up To Date -Lud [ The Names Should Be Fixed But I Am Just Making Sure The Scripts Dont Die]
-    public void UpdateVector( Vector2 pointAUpdate, Vector2 pointBUpdate)
+    public void UpdateVector(Vector2 pointAUpdate, Vector2 pointBUpdate)
     {
         // Updates The Values -Lud
         Vector2 localPos = pointBUpdate - pointAUpdate;
@@ -19,20 +42,9 @@ public class LineCollider : MonoBehaviour
 
         // Rotates The Game Object So It Doesnt Look Fucked -Lud
         gameObject.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(normalUpdate.y, normalUpdate.x) * Mathf.Rad2Deg + 90);
-        gameObject.transform.position = new Vector2((pointAUpdate.x + pointBUpdate.x)/2, (pointAUpdate.y + pointBUpdate.y)/2);
+        gameObject.transform.position = new Vector2((pointAUpdate.x + pointBUpdate.x) / 2, (pointAUpdate.y + pointBUpdate.y) / 2);
     }
-    // Player Veriables -Lud
-    public GameObject PlayerObject;
-    public Rigidbody2D PlayerBody;
-    public CircleCollider2D PlayerCollider;
-    // Line Veriables -Lud
-    public float lenght;
-    public Vector2 pointA;
-    public Vector2 pointB;
-    public Vector2 normal;
-    public static float rigidity;
-    public static float tangentRigidity;
-    public void CheckLineCollison(GameObject PlayerObject, Rigidbody2D PlayerBody, CircleCollider2D PlayerCollider)
+    public void CheckLineCollison()
     {
         Vector2 PlayerPos = PlayerObject.transform.position;
         Vector2 localPlayerPos = (Vector2)gameObject.transform.position - PlayerPos;
@@ -40,9 +52,9 @@ public class LineCollider : MonoBehaviour
         if ((LengthDistance < lenght) && (LengthDistance > 0))
         {
             float distanceAlongNormal = Vector2.Dot(localPlayerPos, new Vector2(normal.y, -normal.x));
-            if(distanceAlongNormal < PlayerCollider.radius)
+            if((distanceAlongNormal < PlayerCollider.radius) && ColliderSide)
             {
-                PlayerObject.transform.position += (Vector3)(new Vector2(normal.y, -normal.x) * (PlayerCollider.radius - distanceAlongNormal));
+                // PlayerObject.transform.position += (Vector3)(new Vector2(normal.y, -normal.x) * (PlayerCollider.radius - distanceAlongNormal));
 
                 float speedAlongNormal = Vector2.Dot(PlayerBody.linearVelocity, new Vector2(normal.y, -normal.x));
                 float speedAlongTangent = Vector2.Dot(PlayerBody.linearVelocity, new Vector2(-normal.x, -normal.y));
@@ -53,6 +65,10 @@ public class LineCollider : MonoBehaviour
                     PlayerBody.linearVelocityX = -(speedAlongNormal * normal.y) * rigidity + (speedAlongTangent * -normal.x) * tangentRigidity;
                     PlayerBody.linearVelocityY = -(speedAlongNormal * -normal.x) * rigidity + (speedAlongTangent * -normal.y) * tangentRigidity;
                 }
+            }
+            else if ((-distanceAlongNormal < PlayerCollider.radius) && !ColliderSide)
+            {
+                // Fuck ._. -Lud
             }
         }
     }
